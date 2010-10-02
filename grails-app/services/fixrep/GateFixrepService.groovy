585bfb4eda331cf40fe74222b98f3ef7d6299a7f
Ian Ibbotson <ianibbo@googlemail.com>
2010-10-02 13:50:46 +0100

    Work on gate client

--- grails-app/services/fixrep/GateFixrepService.groovy
+++ grails-app/services/fixrep/GateFixrepService.groovy
@@ -2,6 +2,9 @@ package fixrep
 
 
 import org.springframework.beans.factory.InitializingBean
+import groovyx.net.http.RESTClient
+import groovy.util.slurpersupport.GPathResult
+import static groovyx.net.http.ContentType.URLENC
 
 class GateFixrepService implements InitializingBean {
 
@@ -26,6 +29,24 @@ class GateFixrepService implements InitializingBean {
     def extract(text) {
       println "extract"
       // Return a list of extracted term information
+      println("Calling GATE enrich");
+      def gateclient = new RESTClient('http://fixrep.k-int.com/fixrepws/extract.xml')
+
+      def response = gateclient.post(
+        //contentType: 'application/xml',
+        //contentType : groovyx.net.http.ContentType.TEXT,
+        // contentType : groovyx.net.http.ContentType.TEXT,
+        contentType : groovyx.net.http.ContentType.XML,
+        requestContentType: groovyx.net.http.ContentType.XML,
+        body: text)
+
+      // println("opencalais response ${response.data.text}")
+
+      // Slurp xml response document - opencalais_response represents the root RDF:rdf element
+      def gate_response = new XmlSlurper().parseText(response.data.text)
+
+      println("response ${gate_response}")
+
       def result = []
     }
 
